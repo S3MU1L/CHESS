b74ad51335ad81a9a4777fd4d5e15a28db5de02a
Samuel Malec <98909801+S3MU1L@users.noreply.github.com>
2022-08-21 12:22:57 +0200

    Update possible.py

--- possible.py
+++ possible.py
@@ -1,32 +1,34 @@
-def king_safe(grid, turn, kingx, kingy):
-    #return list of positions where the king still can go
-    res = []
-    #firstly we find all the positions which are threatened
+def calculate_threats(grid, turn):
+    kingy, kingx = -1, -1
     for y in range(8):
         for x in range(8):
-            if (y != kingy or x != kingx) and grid[y][x][0] != -1:
-                    res.append([y, x])
-            if grid[y][x][1] != turn and grid[y][x][1] != -1:
+            if grid[y][x][1] == turn:
                 if grid[y][x][0] == 5:
-                    res += all_king(x, y, grid)
-                else:
-                    res += calculate_possible([x, y], grid)
-    #now we filter all the bad positions
-    good = []
-    threatened = True if [kingy, kingx] in res else False
+                    kingy, kingx = y, x
+                    break
+    all = [[]]
     for y in range(8):
         for x in range(8):
-            if [y, x] not in res:
-                good.append([y, x])
-                
-    return good, threatened
+            if x != kingx or y != kingy:
+                if grid[y][x][1] != -1:
+                    all.append([y, x])
+            if grid[y][x][1] != -1 and grid[y][x][1] != turn:
+                if grid[y][x][0] == 5:
+                    all += possible_king(x, y, grid, False)
+                elif grid[y][x][0] == 0:
+                    all += possible_pawn(x, y, grid, True)
+                else:
+                    all += calculate_possible([x, y], grid)
+    # print("ALL")
+    # print(all)
+    return all
 
 def calculate_possible(highlighted, grid):
     x = highlighted[0]
     y = highlighted[1]
     piece = grid[y][x][0]
     if piece == 0:
-        return possible_pawn(x, y, grid)
+        return possible_pawn(x, y, grid, False)
     elif piece == 1:
         return possible_knight(x, y, grid)
     elif piece == 2:
@@ -34,59 +36,75 @@ def calculate_possible(highlighted, grid):
     elif piece == 3:
         return possible_rook(x, y, grid)
     elif piece == 4:
-        return possible_queen(x, y, grid)    
-    return possible_king(x, y, grid, grid[y][x][1])[0]
+        return possible_queen(x, y, grid)  
+    
+    return possible_king(x, y, grid, True)
     
     
-def possible_pawn(x, y, grid):
+def possible_pawn(x, y, grid, reduced):
     res = []
-    
     #TODO IMPLEMENT EN PESSANT
     
     #white pawns only move up
-    if grid[y][x][1] == 0:
-        if y == 6:
-            for i in [-1,-2]:
-                if grid[y+i][x][1] != -1:
-                    break
-                else:
-                    res.append([y+i, x])
-                    
-        # if y > 0 and grid[y-1][x][1] != grid[y][x][1]:
-        #     res.append([y-1, x])
-        
-        if y > 0 and x >= 0 and x < 7:
-            if grid[y-1][x+1][1] != -1:
-                if grid[y][x][1] != grid[y-1][x+1][1]:
-                    res.append([y-1,x+1])
-        
-        if y > 0 and x > 0 and x <= 7:
-            if grid[y-1][x-1][1] != -1:
-                if grid[y][x][1] != grid[y-1][x-1][1]:
-                    res.append([y-1, x-1])
-                    
-    else:
-        if y == 1:
-            for i in [1, 2]:
+    turn = grid[y][x][1]
+    if turn == 0:
+        if not reduced:
+            if y == 6:
+                for i in [-1,-2]:
                     if grid[y+i][x][1] != -1:
                         break
                     else:
                         res.append([y+i, x])
-                    
-        # if y < 7 and grid[y+1][x][1] != grid[y][x][1]:
-        #     res.append([y+1, x])
+                        
+            if y > 0 and grid[y-1][x][1] == -1:
+                res.append([y-1, x])
         
+        if y > 0 and x >= 0 and x < 7:
+            if reduced and turn != grid[y-1][x+1][1]:
+                res.append([y-1, x+1])
+                
+            if grid[y-1][x+1][1] != -1:
+                if turn != grid[y-1][x+1][1]:
+                    res.append([y-1, x+1])
+        
+        if y > 0 and x > 0 and x <= 7:
+            if reduced and turn != grid[y-1][x-1][1]:
+                res.append([y-1, x-1])
+                
+            if grid[y-1][x-1][1] != -1:
+                if turn != grid[y-1][x-1][1]:
+                    res.append([y-1, x-1])
+                    
+    else:
+        if not reduced:
+            if y == 1:
+                for i in [1, 2]:
+                        if grid[y+i][x][1] != -1:
+                            break
+                        else:
+                            res.append([y+i, x])
+                        
+            if y < 7 and grid[y+1][x][1] == -1:
+                res.append([y+1, x])
+            
         if y < 7 and x >= 0 and x < 7:
+            if reduced and turn != grid[y+1][x+1][1]:
+                res.append([y+1, x+1])
+                
             if grid[y+1][x+1][1] != -1:
-                if grid[y][x][1] != grid[y+1][x+1][1]:
-                    res.append([y+1,x+1])
+                if turn != grid[y+1][x+1][1]:
+                    res.append([y+1, x+1])
             
         if y < 7 and x > 0 and x <= 7:
+            if reduced and turn != grid[y+1][x-1][1]:
+                res.append([y+1, x-1])
+                
             if grid[y+1][x-1][1] != -1:
-                if grid[y][x][1] != grid[y+1][x-1][1]:
-                    res.append([y+1, x-1])
-                        
+                if turn != grid[y+1][x-1][1]:
+                    res.append([y+1, x-1])                        
     return res
+
+
 def possible_knight(x, y, grid):
     res = []
     values = [-2,2]
@@ -116,9 +134,10 @@ def possible_bishop(x, y, grid):
     y1 = y - 1
     while x1 >= 0 and y1 >= 0:
         if grid[y1][x1][1] != grid[y][x][1]:
-            res.append([y1,x1])
-        if grid[y1][x1] != [-1,-1]:
+            res.append([y1, x1])
+        if grid[y1][x1][1] != -1:
             break
+            
         x1 -= 1
         y1 -= 1
     
@@ -127,8 +146,8 @@ def possible_bishop(x, y, grid):
     y2 = y - 1
     while x2 <= 7 and y2 >= 0:
         if grid[y2][x2][1] != grid[y][x][1]:
-            res.append([y2,x2])
-        if grid[y2][x2] != [-1,-1]:
+            res.append([y2, x2])
+        if grid[y2][x2][1] != -1:
             break
         x2 += 1
         y2 -= 1
@@ -139,7 +158,7 @@ def possible_bishop(x, y, grid):
     while x3 >= 0 and y3 <= 7:
         if grid[y3][x3][1] != grid[y][x][1]:
             res.append([y3, x3])
-        if grid[y3][x3] != [-1,-1]:
+        if grid[y3][x3][1] != -1:
             break
         x3 -= 1
         y3 += 1
@@ -150,7 +169,7 @@ def possible_bishop(x, y, grid):
     while x4 <= 7 and y4 <= 7:
         if grid[y4][x4][1] != grid[y][x][1]:
             res.append([y4, x4])
-        if grid[y4][x4] != [-1,-1]:
+        if grid[y4][x4][1] != -1:
             break
         x4 += 1
         y4 += 1
@@ -164,8 +183,8 @@ def possible_rook(x, y, grid):
     y1 = y
     while x1 >= 0:
         if grid[y1][x1][1] != grid[y][x][1]:
-            res.append([y1,x1])
-        if grid[y1][x1] != [-1,-1]:
+            res.append([y1, x1])
+        if grid[y1][x1][1] != -1:
             break
         x1 -= 1
     
@@ -174,8 +193,8 @@ def possible_rook(x, y, grid):
     y2 = y
     while x2 <= 7:
         if grid[y2][x2][1] != grid[y][x][1]:
-            res.append([y2,x2])
-        if grid[y2][x2] != [-1,-1]:
+            res.append([y2, x2])
+        if grid[y2][x2][1] != -1:
             break
         x2 += 1
         
@@ -185,7 +204,7 @@ def possible_rook(x, y, grid):
     while y3 <= 7:
         if grid[y3][x3][1] != grid[y][x][1]:
             res.append([y3, x3])
-        if grid[y3][x3] != [-1,-1]:
+        if grid[y3][x3][1] != -1:
             break
         y3 += 1
 
@@ -195,7 +214,7 @@ def possible_rook(x, y, grid):
     while y4 >= 0:
         if grid[y4][x4][1] != grid[y][x][1]:
             res.append([y4, x4])
-        if grid[y4][x4] != [-1,-1]:
+        if grid[y4][x4][1] != -1:
             break
         y4 -= 1
     
@@ -203,36 +222,60 @@ def possible_rook(x, y, grid):
 
 
 def possible_queen(x, y, grid):
-    return possible_bishop(x, y, grid) + possible_rook(x, y, grid)
-
-def all_king(x, y, grid):
     res = []
+    res = possible_bishop(x, y, grid) + possible_rook(x, y, grid)
+    return res
+
+def possible_king(x, y, grid, reduced):
+    #TODO IMPLEMENT CASTLING
+    kingx = x
+    kingy = y
+    turn = grid[kingy][kingx][1]
+    res = [[y, x]]
     if y + 1 <= 7:
         for i in [-1,0,1]:
             if x + i >= 0 and x + i <= 7:
-                if grid[y+1][x+i][1] != grid[y][x][1]:
+                if grid[y+1][x+i][1] != turn:
                     res.append([y + 1, x + i])
         
     for i in [-1,1]:
         if x + i >= 0 and x + i <= 7:
-            if grid[y][x+i][1] != grid[y][x][1]:
+            if grid[y][x+i][1] != turn:
                 res.append([y, x + i])
     
     if y - 1 >= 0:
         for i in [-1,0,1]:
             if x + i >= 0 and x + i <= 7:
-                if grid[y-1][x+i][1] != grid[y][x][1]:
+                if grid[y-1][x+i][1] != turn:
                     res.append([y - 1, x + i])
+    if reduced:
+        return check_valid_moves([kingy, kingx], res, turn, grid)
+    # print("ALL POSSIBLE KING MOVES")
+    # print("king color ", grid[kingy][kingx][1])
+    # print(res)
     return res
 
-def possible_king(x, y, grid, turn):
-    positions = all_king(x, y, grid)
-    ans = king_safe(grid, turn, x, y)
-    possible = ans[0]
-    threatened = ans[1]
+def check_valid_moves(highlighted, positions, turn, grid):
+    res = [[]]
+    for y in range(8):
+        for x in range(8):
+            if grid[y][x][1] == turn:
+                if grid[y][x][0] == 5:
+                    kingy, kingx = y, x
+                    break
     
-    res = []
+    x_piece = highlighted[0]
+    y_piece = highlighted[1]
     for pos in positions:
-        if pos in possible:
-            res.append(pos)
-    return res, threatened
+            if pos != []:
+                temp = grid[pos[0]][pos[1]]
+                grid[pos[0]][pos[1]], grid[y_piece][x_piece] = grid[y_piece][x_piece], [-1, -1]
+                if x_piece == kingx and y_piece == kingy:
+                    if pos not in calculate_threats(grid, turn):
+                        res.append(pos)
+                else: 
+                    if [kingy, kingx] not in calculate_threats(grid, turn):
+                        res.append(pos)
+                grid[y_piece][x_piece], grid[pos[0]][pos[1]] = grid[pos[0]][pos[1]], temp
+    
+    return res
